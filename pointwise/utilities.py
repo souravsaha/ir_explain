4aea58f2b4e0ee45442a4652e79d718b025443a8
agarwalh96 <72081185+agarwalh96@users.noreply.github.com>
2024-07-05 14:38:26 +0530

    Update utilities.py
    added 
    from PerturbDocument import PerturbDocument

--- pointwise/utilities.py
+++ pointwise/utilities.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.stats import kendalltau
 import re
+from PerturbDocument import PerturbDocument
+
 
 def get_results_from_index(query, index_searcher, num_docs=10):
     #Perform sparse retrieval to get 'num_docs' number of documents from the index specified by 'index_searcher'
