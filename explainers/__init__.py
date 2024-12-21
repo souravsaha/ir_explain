3436d49eb992b104c60f03a08aa2b7092345f73c
Sourav Saha <souravsaha.juit@gmail.com>
2024-12-21 17:50:15 +0530

    updating explainer __init__.py

--- explainers/__init__.py
+++ explainers/__init__.py
@@ -1,9 +1,11 @@
 from explainers.pointwise.lirme_explainer import LirmePointwiseExplainer
 from explainers.pointwise.base_pointwise import BasePointwiseExplainer
 from explainers.base_explainer import BaseExplainer
+from explainers.pointwise.exs_explainer import EXSPointwiseExplainer
 
 __all__ = [
     "LirmePointwiseExplainer",
+    "EXSPointwiseExplainer",
     "BasePointwiseExplainer",
     "BaseExplainer"
 ]
