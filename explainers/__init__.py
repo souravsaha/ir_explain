a02fb5ea314efd290b87a2b7f268b281e690d0b5
Sourav Saha <souravsaha.juit@gmail.com>
2025-01-24 18:25:41 +0530

    adding initial setup for listwise explainer

--- explainers/__init__.py
+++ explainers/__init__.py
@@ -2,10 +2,12 @@ from explainers.pointwise.lirme_explainer import LirmePointwiseExplainer
 from explainers.pointwise.base_pointwise import BasePointwiseExplainer
 from explainers.base_explainer import BaseExplainer
 #from explainers.pointwise.exs_explainer import EXSPointwiseExplainer
+from explainers.listwise.base_listwise import BaseListwiseExplainer
 
 __all__ = [
     "LirmePointwiseExplainer",
 #    "EXSPointwiseExplainer",
     "BasePointwiseExplainer",
-    "BaseExplainer"
+    "BaseExplainer",
+    "BaseListwiseExplainer"
 ]
